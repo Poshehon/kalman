d3ac127e7e17dff30fccfb0a6170989e42d4b423
Vladislav Maksimov <vladislav.maksimov@math.msu.ru>
2025-05-01 09:06:04 +0300

    Typing

--- filter.py
+++ filter.py
@@ -1,10 +1,29 @@
-class Gauss:
-    def __init__(self, expect = 0, var = 1):
-        self.var = var
-        self.expect = expect
+import math
+import typing
+from pydantic import validate_arguments, BaseModel
+class Gauss(BaseModel):
+    mu: float = 0
+    var: float = 1
+    # Initialize x = Gauss(mu = a, var = b)
+    @property
+    def std(self):
+        return math.sqrt(self.var)
     
     def __repr__(self):
-        return f'N({self.expect}, {self.var})'
+        return f'N({self.mu}, {self.var})'
+    
+    def __add__(self, other):
+        return Gauss(self.mu + other.mu, self.var + other.var)
 
-x = Gauss()
-print(x)
+    def __mul__(self, other):
+        new_mu = (self.var * other.mu + other.var * self.mu ) / (self.var + other.var)
+        new_var = (self.var * other.var) / (self.var + other.var)
+        return Gauss(new_mu, new_var)
+
+@validate_arguments
+def kalman(state: Gauss, measurements : typing.List[Gauss]) -> typing.List[Gauss]:
+    x = Gauss()
+    return [x]
+x = Gauss(mu = 1, var = 2)
+y = Gauss()
+print(x.std)
